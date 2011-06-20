86792275190b5df4a9a2da17e7e3e6d4aaca406d
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2011-06-20 14:21:05 -0300

    Check in some articles validations and SessionsController#new rendering flash, just to get all tests passing for everyone

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,14 @@ class Article < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through => :taggings
 
+  validates_presence_of :title, :url, :body
+  with_options :allow_blank => true do |opt|
+    opt.validates_length_of :title, :minimum => 3
+    opt.validates_format_of :url, :with => URI.regexp
+  end
+
+  attr_accessor :tag_list
+
   def editable_by?(user)
     user.admin? || user == author
   end
