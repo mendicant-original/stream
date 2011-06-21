10516a787e1ebcdd8eb9608f8a93af36612bd967
Walton Hoops <me@waltonhoops.com>
2011-06-20 20:50:49 -0600

    add basic user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :articles, :foreign_key => :author_id
   has_many :authorizations
+
+  attr_accessible :name, :github, :email, :twitter, :location, :about
+
+  def editable_by?(user)
+    user.admin? || user == self
+  end
 end
