3f877b1f4e9c2066ebfccf69d458f2d3ddc58db2
Walton Hoops <me@waltonhoops.com>
2011-06-23 22:16:28 -0600

    Correct tests broken by merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,8 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.last
+    @current_user ||= User.find_by_id(session[:user_id])
   end
-
-  #def current_user
-  #  @current_user ||= User.find_by_id(session[:user_id])
-  #end
 
   def signed_in?
     !!current_user
